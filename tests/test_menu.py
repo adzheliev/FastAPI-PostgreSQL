dd11b8cd97c7611af4143f668589c23ff00a031b
Алан Джелиев <djalan84@mail.ru>
2024-01-30 15:01:37 +0300

    Refactoring: menu tests updated with fixtures to isolate them

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,14 +1,30 @@
 """Tests for Menu CRUD requests"""
 
 from http import HTTPStatus
+
+import pytest
 from httpx import AsyncClient
 
 
 class TestMenu:
     """Main class for Menu tests"""
-    menu_id = None
 
-    async def test_empty_menu_list(
+    """Fixtures for Menu Tests"""
+    @pytest.fixture(scope="function")
+    async def create_menu_fixture(self, ac: AsyncClient):
+        data = {"title": "My menu 1", "description": "My menu description 1"}
+        response = await ac.post("/api/v1/menus/", json=data)
+        assert response.status_code == HTTPStatus.CREATED
+        TestMenu.menu_id = response.json()["id"]
+        yield
+
+    @pytest.fixture(scope="function")
+    async def delete_menu_fixture(self, ac: AsyncClient):
+        yield
+        await ac.delete(f"/api/v1/menus/{TestMenu.menu_id}")
+
+    """Tests"""
+    async def test_get_menus(
             self,
             ac: AsyncClient) -> None:
         """Testing menus list endpoint with empty list"""
@@ -18,88 +34,40 @@ class TestMenu:
         assert response.status_code == HTTPStatus.OK
         assert response.json() == []
 
-    async def test_post_menu(
+    @pytest.mark.usefixtures('create_menu_fixture', 'delete_menu_fixture')
+    async def test_get_menu(
             self,
             ac: AsyncClient) -> None:
-        """Testing menu post endpoint"""
-        data = {
-            "title": "My menu 1",
-            "description": "My menu description 1"
-        }
-        response = await ac.post(
-            "/api/v1/menus/",
-            json=data
-        )
-
-        """Testing status code"""
-        assert response.status_code == HTTPStatus.CREATED
-        TestMenu.menu_id = response.json()["id"]
-
-    async def test_not_empty_menu_list(
-            self,
-            ac: AsyncClient) -> None:
-        """Testing not empty menus list endpoint"""
+        """Testing menu create endpoint"""
         response = await ac.get(
-            "/api/v1/menus/"
+            f"/api/v1/menus/{TestMenu.menu_id}",
         )
 
         """Testing status code"""
         assert response.status_code == HTTPStatus.OK
-
-        """Testing number of menus created code"""
-        assert len(response.json()) == 1
 
         """Testing all fields presence"""
-        assert "title" in response.json()[0]
-        assert "id" in response.json()[0]
-        assert "description" in response.json()[0]
-        assert "submenus_count" in response.json()[0]
-        assert "dishes_count" in response.json()[0]
+        assert "title" in response.json()
+        assert "id" in response.json()
+        assert "description" in response.json()
+        assert "submenus_count" in response.json()
+        assert "dishes_count" in response.json()
 
         """Testing all fields values"""
-        assert response.json()[0]["id"] == TestMenu.menu_id
-        assert response.json()[0]["title"] == "My menu 1"
-        assert response.json()[0]["description"] == "My menu description 1"
-        assert response.json()[0]["submenus_count"] == 0
-        assert response.json()[0]["dishes_count"] == 0
+        assert response.json()["id"] == TestMenu.menu_id
+        assert response.json()["title"] == "My menu 1"
+        assert response.json()["description"] == "My menu description 1"
+        assert response.json()["submenus_count"] == 0
+        assert response.json()["dishes_count"] == 0
 
         """Testing all fields values types"""
-        assert type(response.json()[0]["id"]) == str
-        assert type(response.json()[0]["title"]) == str
-        assert type(response.json()[0]["description"]) == str
-        assert type(response.json()[0]["submenus_count"]) == int
-        assert type(response.json()[0]["dishes_count"]) == int
+        assert type(response.json()["id"]) == str
+        assert type(response.json()["title"]) == str
+        assert type(response.json()["description"]) == str
+        assert type(response.json()["submenus_count"]) == int
+        assert type(response.json()["dishes_count"]) == int
 
-    async def test_post_menu_wit_same_parameters(
-            self,
-            ac: AsyncClient) -> None:
-        """Testing menu post endpoint with duplicate of menu"""
-        data = {
-            "title": "My menu 1",
-            "description": "My menu description 1"
-        }
-        response = await ac.post(
-            "/api/v1/menus/",
-            json=data
-        )
-
-        """Testing status code"""
-        assert response.status_code == HTTPStatus.CONFLICT
-
-    async def test_number_of_menus_in_menus_list_after_duplicate_post(
-            self,
-            ac: AsyncClient) -> None:
-        """Testing number of menus in menus list after duplicate post"""
-        response = await ac.get(
-            "/api/v1/menus/"
-        )
-
-        """Testing status code"""
-        assert response.status_code == HTTPStatus.OK
-
-        """Testing number of menus created"""
-        assert len(response.json()) == 1
-
+    @pytest.mark.usefixtures('create_menu_fixture', 'delete_menu_fixture')
     async def test_patch_menu(
             self,
             ac: AsyncClient) -> None:
@@ -111,17 +79,6 @@ class TestMenu:
         response = await ac.patch(
             f"/api/v1/menus/{TestMenu.menu_id}",
             json=data
-        )
-
-        """Testing status code"""
-        assert response.status_code == HTTPStatus.OK
-
-    async def test_specific_patched_menu(
-            self,
-            ac: AsyncClient) -> None:
-        """Testing patched menu"""
-        response = await ac.get(
-            f"/api/v1/menus/{TestMenu.menu_id}"
         )
 
         """Testing status code"""
@@ -162,38 +119,11 @@ class TestMenu:
         """Testing status code"""
         assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-    async def test_patch_not_existing_menu(
+    @pytest.mark.usefixtures('create_menu_fixture')
+    async def test_delete_menu(
             self,
             ac: AsyncClient) -> None:
-        """Testing non existing menu patch"""
-        data = {
-            "title": "My once again updated menu 1",
-            "description": "My once again updated menu description 1"
-        }
-        response = await ac.patch(
-            "/api/v1/menus/non_existing_menu",
-            json=data
-        )
-
-        """Testing status code"""
-        assert response.status_code == HTTPStatus.NOT_FOUND
-
-    async def test_delete_not_existing_menu(
-            self,
-            ac: AsyncClient) -> None:
-        """Testing non existing menu delete"""
-
-        response = await ac.delete(
-            "/api/v1/menus/non_existing_menu"
-        )
-
-        """Testing status code"""
-        assert response.status_code == HTTPStatus.NOT_FOUND
-
-    async def test_delete_existing_menu(
-            self,
-            ac: AsyncClient) -> None:
-        """Testing existing menu delete"""
+        """Testing menu delete"""
 
         response = await ac.delete(
             f"/api/v1/menus/{TestMenu.menu_id}"
